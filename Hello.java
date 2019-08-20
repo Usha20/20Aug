6f34232aacea605a607f1a545d89a65218e68954
usha <ushavislavth8742@gmail.com>
2019-08-20 19:45:32 +0530

    A.java is added

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package dto;
+public class Hello
+{
+ public static void main(String[] args)
+ {
+   System.out.println("My first program!");
+ }
+}
+
+   
---
+++ RemoveSpace.java
@@ -0,0 +1,12 @@
+public class Concatination
+{
+ public static void main(String[] args)
+ {
+  String s1 = "DevOps Engineer";
+  String s2 = "java";
+  String s3 = s2 + "developer";
+  System.out.println(s1==s3);
+ }
+}
+
+   
